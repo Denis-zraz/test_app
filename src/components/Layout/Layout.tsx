f16ffb9c6286eaa8bf559b52d179951fd75c0c69
Denis-zraz <den.zrazhevskii@mail.ru>
2025-06-26 12:04:03 +0300

    установка tailwind, создание Header

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,7 +1,13 @@
 // import React from 'react'
 
+import { Outlet } from 'react-router-dom';
+import Header from '../Header';
+
 export default function Layout() {
-  return (
-    <div>Layout</div>
-  )
+    return (
+        <section className='w-[1440px] h-[1080px] m-auto'>
+            <Header />
+            <Outlet />
+        </section>
+    );
 }
